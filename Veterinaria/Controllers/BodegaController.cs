f5a121b963e05f0c9f59560c32e8aa78a31e8c6a
Orrv2904 <Orrv2904@gmail.com>
2022-10-17 15:51:02 -0600

    Agregado el controlador de Bodega

---
+++ Veterinaria/Controllers/BodegaController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Controllers
+{
+    public class BodegaController
+    {
+    }
+}
